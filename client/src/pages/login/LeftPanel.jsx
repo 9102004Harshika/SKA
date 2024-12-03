3b8e27a33bbfe942bf265decaa8f35bbb8934a43
Abhay Kumar Das <Abhay744das@gmail.com>
2024-12-03 19:03:26 +0530

    bg form images added

--- client/src/pages/login/LeftPanel.jsx
+++ client/src/pages/login/LeftPanel.jsx
@@ -8,7 +8,7 @@ function LeftPanel() {
         alt="Logo"
         className="mb-6 w-32 h-32 mx-auto"
       />
-      <h1 className="text-4xl font-highlight font-bold mb-4">Welcome Back!</h1>
+      <h1 className="text-4xl font-highligh font-bold mb-4">Welcome Back!</h1>
       <p className="text-lg">
         "The journey of a thousand miles begins with a single step." <br />
         <span className="font-semibold">- Rahul Singh</span>
