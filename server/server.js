f1c764da872f6d56ffbd66b21869fa2ff6ecbc28
Abhay Kumar Das <Abhay744das@gmail.com>
2024-12-13 21:11:40 +0530

    nodemon server run successfully added

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const DB_URL =
+  "mongodb+srv://ShreeKalamAcademy:69696969@ska.qi4ln.mongodb.net/?retryWrites=true&w=majority&appName=SKA";
+
+// MongoDB Connection
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+  });
+
+// Define a sample route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
