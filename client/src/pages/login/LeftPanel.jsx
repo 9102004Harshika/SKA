f7a3591d56e6a275011b63b351682dfa85babc2b
Abhay Kumar Das <Abhay744das@gmail.com>
2024-12-15 11:05:53 +0530

    logo added

--- client/src/pages/login/LeftPanel.jsx
+++ client/src/pages/login/LeftPanel.jsx
@@ -1,13 +1,10 @@
 import React from "react";
+import logo from "./../../images/logo.jpg";
 
 function LeftPanel() {
   return (
     <div className="text-center flex flex-col justify-center p-16 text-background">
-      <img
-        src="https://media.istockphoto.com/vectors/vector-cartoon-boy-reading-a-bookboy-having-an-idea-raised-his-hand-vector-id926748210?k=6&m=926748210&s=170667a&w=0&h=U94PeQmvVF1TV-YkLXRi7ttvxmS0o41gRc3kpeHOt2Y="
-        alt="Logo"
-        className="mb-6 w-32 h-32 mx-auto"
-      />
+      <img src={logo} alt="Logo" className="mb-6 w-32 h-32 mx-auto" />
       <h1 className="text-4xl font-highlight font-bold mb-4">Welcome Back!</h1>
       <p className="text-lg">
         "The journey of a thousand miles begins with a single step." <br />
