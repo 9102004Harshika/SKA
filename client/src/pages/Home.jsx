a72ecfd293004d172fc945a180eafc7bcd6eecfd
Abhay Kumar Das <Abhay744das@gmail.com>
2024-12-22 17:30:17 +0530

    no big changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,9 +8,11 @@ const Home = () => {
       <Navbar />
       <Carousel />
 
-      <h1>Top Picks For you...</h1>
-      <h1>One Shot Videos...</h1>
-      <h1>Suggested Videos...</h1>
+      <h1>Resume wheres left...(Videos Pending) </h1>
+      <h1>Top Picks For you...(Courses shown as carousel) </h1>
+      <h1>One Shot Videos... (Videos) </h1>
+      <h1>Small Videos... (Videos) </h1>
+      <h1>Featured Courses... (Courses shown as carousel)</h1>
     </div>
   );
 };
