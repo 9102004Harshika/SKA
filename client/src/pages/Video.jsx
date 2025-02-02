3e8c1b4de69b8bcb9c53d2a2ffc289519e753130
Abhay Kumar Das <Abhay744das@gmail.com>
2025-02-02 14:24:50 +0530

    video link changed for convenience

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -4,8 +4,8 @@ import VideoPlayer from "../components/VideoPlayer";
 const Video = () => {
   return (
     <div>
-      <h1 className="text-center text-2xl font-bold">React Video Player</h1>
-      <VideoPlayer videoSrc="https://cdn.pixabay.com/video/2024/11/07/240330_large.mp4" />
+      <h1 className="text-center text-2xl my-16 font-bold">Sample Video</h1>
+      <VideoPlayer videoSrc="https://cdn.pixabay.com/video/2020/01/05/30902-383991325_large.mp4" />
     </div>
   );
 };
