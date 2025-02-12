567802f362aa472e0fdff2629a1439fa59a6c237
Abhay Kumar Das <Abhay744das@gmail.com>
2025-02-12 20:43:49 +0530

    input changes

--- client/src/pages/admin/AddCoursePage.jsx
+++ client/src/pages/admin/AddCoursePage.jsx
@@ -5,11 +5,30 @@ import FileUploader from "../../ui/fileUploader";
 
 const AddCoursePage = () => {
   return (
-    <div>
+    <div className="my-2">
+      <h2 className="mb-10 font-header text-3xl font-semibold text-center ">
+        Create New Course
+      </h2>
       <TextInput label="Name" type="text" />
-      <TextAreaInput label="Description" type="text" />
       <TextInput label="Tutor" type="text" />
       <TextInput label="Videos" type="number" />
+      <TextAreaInput label="Description" type="text" />
+      <TextAreaInput
+        label="Course Description"
+        name="courseDescription"
+        // value={courseData.courseDescription}
+        // onChange={handleChange}
+        className=""
+        required
+      />
+      <TextAreaInput
+        label="About Course"
+        name="aboutCourse"
+        // value={courseData.aboutCourse}
+        // onChange={handleChange}
+        className=""
+        required
+      />
       <FileUploader label="Videos" />
     </div>
   );
