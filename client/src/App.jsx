fd5554c58de6783248ae243fa1757d8b013a9c32
Abhay Kumar Das <Abhay744das@gmail.com>
2025-02-20 08:15:51 +0530

    Added new blank files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import TermsConditions from "./components/Terms&Conditions";
 import NotFound from "./pages/NotFound";
 import Unauth from "./pages/Unauth";
 import AddNotesPage from "./pages/admin/AddNotesPage";
+import UpdateNotesPage from "./pages/admin/UpdateNotesPage";
 import Notes from "./pages/Notes";
 import CircularProgress from "./ui/progressBar";
 import Modal from "./components/Modal";
@@ -62,10 +63,12 @@ const App = () => {
               <Route path="update" element={<AddCourseForm />} />
               {/* <Route path="delete" element={<DeleteCourse />} /> */}
             </Route>
+
+            {/* notes section of admin */}
             <Route path="/admin/notes" element={<NotesAdminLayout />}>
               {/* <Route index element={<NotesAdminDashboard />} /> */}
               <Route path="create" element={<AddNotesPage />} />
-              {/* <Route path="update" element={<UpdateNotesPage />} /> */}
+              <Route path="update" element={<UpdateNotesPage />} />
               {/* <Route path="delete" element={<DeleteNotesPage />} /> */}
             </Route>
 
---
+++ client/src/pages/admin/DeleteNotesPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteNotesPage = () => {
+  return <div>DeleteNotesPage</div>;
+};
+
+export default DeleteNotesPage;
